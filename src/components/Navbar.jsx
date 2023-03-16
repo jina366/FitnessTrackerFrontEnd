073b7cd22ed69867db598dfadac0642eb14171b0
Jieun Kim <jieunkim1379@gmail.com>
2023-03-16 15:34:33 -0400

    added tabs in navbar and started Routines component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { NavbarUser } from './';
 
-const Navbar = ({ setToken }) => {
+const Navbar = ({ setToken, token }) => {
   return (
     <div id="navbar">
       <div id="navbarLogo">Logo</div>
-      <div id="navbarTabs">Tabs</div>
+      <div id="navbarTabs">
+        <div id="navbarTabs-container">
+          <h2>Home</h2>
+          <h2>Routines</h2>
+          {token ? <h2>My Routines</h2>: null}
+          <h2>Activities</h2>
+        </div>
+      </div>
       <div id="navbarUser">
         <NavbarUser setToken={setToken} />
       </div>
